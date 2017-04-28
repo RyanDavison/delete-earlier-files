fc42473dfbae55af7210e93c708afe3c906f1c0e
Ryan Davison <ryandavison11@gmail.com>
2017-04-28 08:59:23 -0600

    Add days offset test to getStartTimestamp

--- test/delete-earlier-files-test.js
+++ test/delete-earlier-files-test.js
@@ -4,10 +4,17 @@ const moment = require('moment');
 const DeleteEarlierFiles = require('../index');
 
 describe('DeleteEarlierFiles', function(){
-    it('DeleteEarlierFiles.getStartTimestamp() returns the current month, day and year', function(){
-        const DeleteEarlier = new DeleteEarlierFiles('./files');
-        const start = moment().subtract(0, 'days').format("DD-MM-YYYY");
-        console.log('start time is', start)
-        expect(DeleteEarlier.getStartTimestamp()).to.equal(start);
+    it(`getStartTimestamp() returns the current month, day and year when no days
+        offset is passed to DeleteEarlierFiles()`, function(){
+            const DeleteEarlier = new DeleteEarlierFiles('./files');
+            const start = moment().subtract(0, 'days').format("DD-MM-YYYY");
+            expect(DeleteEarlier.getStartTimestamp()).to.equal(start);
+    });
+
+    it(`getStartTimestamp() returns the month, day and year from two days ago
+        when a days offset of 2 is passed to DeleteEarlierFiles`, function(){
+            const DeleteEarlier = new DeleteEarlierFiles('./files', 2);
+            const start = moment().subtract(2, 'days').format("DD-MM-YYYY");
+            expect(DeleteEarlier.getStartTimestamp()).to.equal(start);
     });
 })
